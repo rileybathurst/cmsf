3842655212940e9a1b8c6ff6a1fe72b25b534fcf
Riley Bathurst <riley@rileybathurst.com>
2021-08-03 15:39:35 -0700

    events

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,6 +1,16 @@
 wp.domReady( () => {
 
 	wp.blocks.registerBlockStyle(
+		'core/group',
+		[
+			{
+				name: 'alignfull',
+				label: 'alignfull',
+			}
+		]
+	);
+
+	wp.blocks.registerBlockStyle(
 		'core/image',
 		[
 			{
---
+++ pages/Cordell Marine Sanctuary Foundation.php
@@ -0,0 +1,256 @@
+<!-- I have to do the formatting manually -->
+
+<!-- wp:group {"align":"full","style":{"color":{"background":"#066b7e"}},"textColor":"white","className":"cmsf-hero"} -->
+<div class="wp-block-group alignfull cmsf-hero has-white-color has-text-color has-background" style="background-color:#066b7e"><!-- wp:columns {"align":"full","className":"is-style-fullwidth-wide"} -->
+	<div class="wp-block-columns alignfull is-style-fullwidth-wide">
+		<!-- wp:column {"width":"50%"} -->
+		<div class="wp-block-column" style="flex-basis:50%">
+			<!-- wp:heading -->
+			<h2>Supporting the Cordell Bank National Marine Sanctuary in:</h2>
+			<!-- /wp:heading -->
+
+			<!-- wp:list -->
+			<ul>
+				<li>Scientific research programs, studying climate change, ocean acidification, and marine life</li>
+				<li>Education, outreach, and increasing public awareness of the Sanctuary and it’s programs</li>
+				<li>Conservation and protection of the Sanctuary resources, including whales, seabirds and corals</li>
+			</ul>
+			<!-- /wp:list -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"50%"} -->
+		<div class="wp-block-column" style="flex-basis:50%">
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:image {"id":710,"sizeSlug":"large","linkDestination":"none"} -->
+	<figure class="wp-block-image size-large"><img src="https://cordellfoundation.org/wp-content/uploads/2019/07/Rosy_Rock_Fish-e1562821588235-1024x675.jpg" alt="" class="wp-image-710"/></figure>
+	<!-- /wp:image -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"color":{"background":"#066b7e"}},"textColor":"white"} -->
+<div class="wp-block-group alignfull has-white-color has-text-color has-background" style="background-color:#066b7e">
+	<!-- wp:columns -->
+	<div class="wp-block-columns">
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image {"id":546,"sizeSlug":"large","linkDestination":"none"} -->
+			<figure class="wp-block-image size-large"><img src="https://cordellfoundation.org/wp-content/uploads/2015/10/map_expanded_boundaries_6-15_big-749x1024.jpg" alt="" class="wp-image-546"/></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:paragraph -->
+			<p>The sanctuary was established in 1989 to protect and preserve the extraordinary ecosystem, including marine birds, mammals, and other natural resources of Cordell Bank and its surrounding waters. The Sanctuary encompasses brilliant and diverse marine life, above and below the water.</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph -->
+			<p><a href="#">Learn More</a></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph -->
+			<p><a href="#">View a Story Map of the Cordell Bank NMS</a></p>
+			<!-- /wp:paragraph -->
+		
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"color":{"background":"#facc02"}},"textColor":"black"} -->
+<div class="wp-block-group alignfull has-black-color has-text-color has-background" style="background-color:#facc02">
+	<!-- wp:columns -->
+	<div class="wp-block-columns">
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:heading {"level":3} -->
+			<h3>Statement from the Board of Directors</h3>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>Racism, bias, and inequity for Black, Indigenous, and people of color (BIPOC) plagues communities and reaches deep to all aspects of society, including ocean sciences and conservation. Our core mission at the Cordell Marine Sanctuary Foundation (CMSF) involves supporting science &amp; education at Cordell Bank National Marine Sanctuary (CBNMS) to better understand the past, present, and future of the unique and special place that is CBNMS. This understanding is directly related to climate change and the health of our planet and its inhabitants. Systemic oppression, climate change and environmental protection are deeply intertwined; and disproportionately and drastically affect BIPOC communities, locally and around the world. CMSF stands in solidarity with Black Lives Matter, and all those protesting racial injustice. We commit to doing our part to dismantle white supremacy and to help build a more diverse and inclusive science, education, and conservation program at CBNMS. We call for a future that puts the needs of justice and community first.</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:image {"id":247,"sizeSlug":"large","linkDestination":"none","className":"is-style-desatured-multiply"} -->
+			<figure class="wp-block-image size-large is-style-desatured-multiply"><img src="https://cordellfoundation.org/wp-content/uploads/2014/05/involved2.jpg" alt="" class="wp-image-247"/></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns">
+	<!-- wp:column -->
+	<div class="wp-block-column">
+		<!-- wp:columns {"className":"flex-columns"} -->
+		<div class="wp-block-columns flex-columns">
+			<!-- wp:column -->
+			<div class="wp-block-column">
+				<!-- wp:image {"id":5840,"sizeSlug":"large","linkDestination":"none"} -->
+				<figure class="wp-block-image size-large"><img src="https://cmsf.tahoewebshop.com/wp-content/uploads/2021/06/seahorse.png" alt="" class="wp-image-5840"/></figure>
+				<!-- /wp:image -->
+			</div>
+			<!-- /wp:column -->
+
+			<!-- wp:column -->
+			<div class="wp-block-column">
+				<!-- wp:group -->
+				<div class="wp-block-group">
+					
+					<!-- wp:heading {"level":3} -->
+					<h3>CMSF TV</h3>
+					<!-- /wp:heading -->
+
+					<!-- wp:paragraph -->
+					<p><a href="#">More Videos</a></p>
+					<!-- /wp:paragraph -->
+				</div>
+				<!-- /wp:group -->
+			</div>
+			<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column"></div>
+	<!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
+
+<!-- wp:embed {"url":"https://www.youtube.com/watch?v=dqvMk6op6So","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://www.youtube.com/watch?v=dqvMk6op6So
+</div></figure>
+<!-- /wp:embed -->
+
+<!-- wp:paragraph -->
+<p>Learn more about the organisms that call the sanctuary home.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:embed {"url":"https://www.youtube.com/watch?v=KSiTNyZqG-Q","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://www.youtube.com/watch?v=KSiTNyZqG-Q
+</div></figure>
+<!-- /wp:embed -->
+
+<!-- wp:columns {"className":"flex-columns"} -->
+<div class="wp-block-columns flex-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":5856,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://cmsf.tahoewebshop.com/wp-content/uploads/2021/06/play.png" alt="" class="wp-image-5856"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Cordell Bank: A National Treasure</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:embed {"url":"https://www.youtube.com/watch?v=IHs6EmzxfhI","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-4-3 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-4-3 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://www.youtube.com/watch?v=IHs6EmzxfhI
+</div></figure>
+<!-- /wp:embed -->
+
+<!-- wp:columns {"className":"flex-columns"} -->
+<div class="wp-block-columns flex-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":5856,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://cmsf.tahoewebshop.com/wp-content/uploads/2021/06/play.png" alt="" class="wp-image-5856"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Creatures of the Cordell Bank</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"align":"full","style":{"color":{"background":"#facc02"}},"textColor":"black"} -->
+<div class="wp-block-group alignfull has-black-color has-text-color has-background" style="background-color:#facc02"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3>News</h3>
+<!-- /wp:heading -->
+
+<!-- wp:latest-posts /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":296,"sizeSlug":"large","linkDestination":"none","className":"is-style-desatured-multiply"} -->
+<figure class="wp-block-image size-large is-style-desatured-multiply"><img src="https://cordellfoundation.org/wp-content/uploads/2014/06/GM_100910_0024-1024x683.jpg" alt="" class="wp-image-296"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:jetpack/tiled-gallery {"align":"full","columnWidths":[["20.88606","55.37927","23.73466"],["66.56223","33.43777"],["21.47574","21.60234","56.92192"]],"ids":[349,116,710,300,296,151,310,152]} -->
+<div class="wp-block-jetpack-tiled-gallery alignfull is-style-rectangular"><div class="tiled-gallery__gallery"><div class="tiled-gallery__row"><div class="tiled-gallery__col" style="flex-basis:20.88606%"><figure class="tiled-gallery__item"><img alt="" data-height="1920" data-id="349" data-link="https://cmsf.tahoewebshop.com/stock_omca/" data-url="https://cmsf.tahoewebshop.com/wp-content/uploads/2014/06/Stock_OMCA-scaled.jpg" data-width="2560" src="https://i1.wp.com/cmsf.tahoewebshop.com/wp-content/uploads/2014/06/Stock_OMCA-scaled.jpg?ssl=1" data-amp-layout="responsive"/></figure></div><div class="tiled-gallery__col" style="flex-basis:55.37927%"><figure class="tiled-gallery__item"><img alt="" data-height="450" data-id="116" data-link="https://cmsf.tahoewebshop.com/slide1/" data-url="https://cmsf.tahoewebshop.com/wp-content/uploads/2014/05/slide1.jpg" data-width="1600" src="https://i0.wp.com/cmsf.tahoewebshop.com/wp-content/uploads/2014/05/slide1.jpg?ssl=1" data-amp-layout="responsive"/></figure></div><div class="tiled-gallery__col" style="flex-basis:23.73466%"><figure class="tiled-gallery__item"><img alt="" data-height="712" data-id="710" data-link="https://cmsf.tahoewebshop.com/sanctuary-news/events-meetings/rosy_rock_fish/" data-url="https://cmsf.tahoewebshop.com/wp-content/uploads/2019/07/Rosy_Rock_Fish-e1562821588235.jpg" data-width="1080" src="https://i2.wp.com/cmsf.tahoewebshop.com/wp-content/uploads/2019/07/Rosy_Rock_Fish-e1562821588235.jpg?ssl=1" data-amp-layout="responsive"/></figure></div></div><div class="tiled-gallery__row"><div class="tiled-gallery__col" style="flex-basis:66.56223%"><figure class="tiled-gallery__item"><img alt="" data-height="807" data-id="300" data-link="https://cmsf.tahoewebshop.com/jh2_100910_0028/" data-url="https://cmsf.tahoewebshop.com/wp-content/uploads/2014/06/JH2_100910_0028.jpg" data-width="1200" src="https://i2.wp.com/cmsf.tahoewebshop.com/wp-content/uploads/2014/06/JH2_100910_0028.jpg?ssl=1" data-amp-layout="responsive"/></figure></div><div class="tiled-gallery__col" style="flex-basis:33.43777%"><figure class="tiled-gallery__item"><img alt="" data-height="800" data-id="296" data-link="https://cmsf.tahoewebshop.com/henricia-spp-na-echinasteridae/" data-url="https://cmsf.tahoewebshop.com/wp-content/uploads/2014/06/GM_100910_0024.jpg" data-width="1200" src="https://i0.wp.com/cmsf.tahoewebshop.com/wp-content/uploads/2014/06/GM_100910_0024.jpg?ssl=1" data-amp-layout="responsive"/></figure><figure class="tiled-gallery__item"><img alt="Oakland Museum exhibit on Cordell Bank" data-height="458" data-id="151" data-link="https://cmsf.tahoewebshop.com/oakland-museum/" data-url="https://cmsf.tahoewebshop.com/wp-content/uploads/2014/05/oakland-museum.jpg" data-width="687" src="https://i0.wp.com/cmsf.tahoewebshop.com/wp-content/uploads/2014/05/oakland-museum.jpg?ssl=1" data-amp-layout="responsive"/></figure></div></div><div class="tiled-gallery__row"><div class="tiled-gallery__col" style="flex-basis:21.47574%"><figure class="tiled-gallery__item"><img alt="" data-height="1696" data-id="310" data-link="https://cmsf.tahoewebshop.com/db_cbnms_20070909_jls_063/" data-url="https://cmsf.tahoewebshop.com/wp-content/uploads/2014/06/db_CBNMS_20070909_JLS_063.jpg" data-width="2544" src="https://i2.wp.com/cmsf.tahoewebshop.com/wp-content/uploads/2014/06/db_CBNMS_20070909_JLS_063.jpg?ssl=1" data-amp-layout="responsive"/></figure></div><div class="tiled-gallery__col" style="flex-basis:21.60234%"><figure class="tiled-gallery__item"><img alt="" data-height="224" data-id="152" data-link="https://cmsf.tahoewebshop.com/museum-left/" data-url="https://cmsf.tahoewebshop.com/wp-content/uploads/2014/05/museum-left.jpg" data-width="338" src="https://i1.wp.com/cmsf.tahoewebshop.com/wp-content/uploads/2014/05/museum-left.jpg?ssl=1" data-amp-layout="responsive"/></figure></div></div></div></div>
+<!-- /wp:jetpack/tiled-gallery -->
+
+<!-- wp:group {"align":"full","backgroundColor":"seahorse","textColor":"black"} -->
+<div class="wp-block-group alignfull has-black-color has-seahorse-background-color has-text-color has-background"><!-- wp:image {"id":5911,"sizeSlug":"large","linkDestination":"media"} -->
+<figure class="wp-block-image size-large"><a href="https://cordellfoundation.org/wp-content/uploads/2021/06/migrate.jpg"><img src="https://cordellfoundation.org/wp-content/uploads/2021/06/migrate-1024x604.jpg" alt="global map showing migratory species coming to the CMSF" class="wp-image-5911"/></a><figcaption>A sample of some of the animals that migrate to Cordell Bank National Marine Sanctuary</figcaption></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"color":{"background":"#066b7e"}},"textColor":"white","className":"cmsf-zero"} -->
+<div class="wp-block-group alignfull cmsf-zero has-white-color has-text-color has-background" style="background-color:#066b7e"><!-- wp:columns {"align":"full","className":"is-style-fullwidth-wide"} -->
+<div class="wp-block-columns alignfull is-style-fullwidth-wide"><!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:heading {"level":3} -->
+<h3><a href="https://cordellfoundation.org/whale-spotter/">Whale Spotter Application – Preventing Ship Strikes</a></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Cordell Marine Sanctuary Foundation and others initiated a whale spotter program to help reduce ship strikes.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+<!-- wp:heading {"level":4} -->
+<h4><a href="https://cordellfoundation.org/hypoxia-studies-in-cordell-bank-national-marine-sanctuary-funded-by-cmsf/">Hypoxia Monitoring Moorings Deployed at Cordell Bank</a></h4>
+<!-- /wp:heading --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:image {"id":152,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://cordellfoundation.org/wp-content/uploads/2014/05/museum-left.jpg" alt="" class="wp-image-152"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
